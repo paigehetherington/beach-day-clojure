302192872a3089fb950117244b501ebaa53f5560
Paige Hetherington <herbmama@gmail.com>
2016-03-25 16:25:20 -0400

    Add table and add photo and search by name option

--- src/beach_day_clojure/core.clj
+++ src/beach_day_clojure/core.clj
@@ -3,12 +3,17 @@
             [compojure.core :as c]
             [hiccup.core :as h]
             [ring.middleware.params :as p]
+            ;[clojure.pprint :as pp]
             [ring.util.response :as r])
   (:gen-class))
 
 (defonce server (atom nil))
 
 (defonce surfers (atom {})) ; HM
+
+(add-watch surfers :save-to-disk
+  (fn [_ _ _ _]
+    (spit "surfers.edn" (pr-str @surfers))))
 
 (c/defroutes app
   (c/GET "/" request
@@ -20,7 +25,8 @@
                   [:option {:disabled "disabled" :selected "selected"} "Board Type"]
                   [:option {:value "Shortboard"} "Shortboard"]
                   [:option {:value "Longboard"} "Longboard"]
-                  [:option {:value "Funboard"} "Funboard"]]
+                  [:option {:value "Funboard"} "Funboard"]
+                  [:option {:value "Fish"} "Fish"]]
                 [:input {:type "text" :placeholder "Country" :name "country"}]
                 [:input {:type "text" :placeholder "Home Break" :name "home-break"}]
                 [:select {:name "ability-level"}
@@ -29,10 +35,30 @@
                   [:option {:value "Intermediate"} "Intermediate"]
                   [:option {:value "Advanced"} "Advanced"]]
                 [:input {:type "number" :placeholder "Number of Waves" :name "number-of-waves"}]
+                [:input {:type "text" :placeholder "Image URL" :name "image-url"}]
                 [:button {:type "Submit"} "Add surfer"]]
-              [:ol
+              [:form {:action "/get-surfer" :method "get"}
+                [:input {:type "text" :placeholder "Enter Name" :name "name"}]
+                [:button {:type "submit"} "Search"]]
+              [:table
+               [:tr 
+                [:th "Name"]
+                [:th "Board Type"]
+                [:th "Country"]
+                [:th "Home Break"]
+                [:th "Ability Level"]
+                [:th "Number of Waves"]
+                [:th "Photo"]]
+                
                (map (fn [surfer]
-                      [:li (:name surfer) " " (:board-type surfer) " " (:country surfer) " " (:home-break surfer) " " (:ability-level surfer) " " (:number-of-waves surfer)])
+                      [:tr
+                       [:td (:name surfer)]
+                       [:td (:board-type surfer)] 
+                       [:td (:country surfer)] 
+                       [:td (:home-break surfer)] 
+                       [:td (:ability-level surfer)] 
+                       [:td (:number-of-waves surfer)]
+                       [:td [:img {:src (:image-url surfer) :width 100 :height 100}]]])
                   (vals @surfers))]]]))
 
   (c/POST "/add-surfer" request
@@ -42,15 +68,39 @@
           home-break (get (:params request) "home-break")
           number-of-waves (get (:params request) "number-of-waves")
           ability-level (get (:params request) "ability-level")
-          surfer (hash-map :name name :board-type board-type :country country :home-break home-break :ability-level ability-level :number-of-waves number-of-waves)]
+          image-url (get (:params request) "image-url")
+          surfer (hash-map :name name :board-type board-type :country country :home-break home-break :ability-level ability-level :number-of-waves number-of-waves :image-url image-url)]
       (swap! surfers assoc name surfer) ; surfers is HM
       (r/redirect "/")))
 
   (c/GET "/get-surfer" request
-    (let [name (get (:params request "name"))
-          surfer (get @surfers name)])))
+    (let [name (get (:params request) "name")
+          surfer (get @surfers name)]
+      (h/html
+        [:html
+         [:body
+          [:br] 
+          (:name surfer)
+          [:br] 
+          (:board-type surfer) 
+          [:br] 
+          (:country surfer) 
+          [:br] 
+          (:home-break surfer) 
+          [:br] 
+          (:ability-level surfer) 
+          [:br] 
+          (:number-of-waves surfer)
+          [:br] 
+          [:img {:src (:image-url surfer) :width 100 :height 100}]]]))))
+          
 
 (defn -main []
+    (try
+      (let [surfers-str (slurp "surfers.edn")
+            surfers-vec (read-string surfers-str)]
+         (reset! surfers surfers-vec))
+     (catch Exception _))
   (when @server
     (.stop @server))
   (reset! server (j/run-jetty (p/wrap-params app) {:port 3000 :join? false})))
